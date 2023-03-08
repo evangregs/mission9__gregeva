b2a08cf5b8dd62322a8d5a1e9c649b31b35ea0af
gregeva <evang0911@gmail.com>
2023-03-08 15:56:40 -0700

    mission 10 2nd push

--- onlineBookstore/Infrastructure/PaginationTagHelper.cs
+++ onlineBookstore/Infrastructure/PaginationTagHelper.cs
@@ -28,7 +28,10 @@ namespace onlineBookstore.Infrastructure
 
         public PageInfo PageSkip { get; set; }
         public string PageAction { get; set; }
-
+        public string PageClass { get; set; }
+        public bool PageClassesEnabled { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
         public override void Process(TagHelperContext thc, TagHelperOutput tho)
         {
             IUrlHelper uh = uhf.GetUrlHelper(vc);
@@ -39,6 +42,13 @@ namespace onlineBookstore.Infrastructure
                 TagBuilder tb = new TagBuilder("a");
 
                 tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+
+                if(PageClassesEnabled)
+                {
+                    tb.AddCssClass(PageClass);
+                    tb.AddCssClass(i == PageSkip.CurrentPage ? PageClassSelected : PageClassNormal);
+                }
+                tb.AddCssClass(PageClass);
                 tb.InnerHtml.Append(i.ToString());
 
                 final.InnerHtml.AppendHtml(tb);
