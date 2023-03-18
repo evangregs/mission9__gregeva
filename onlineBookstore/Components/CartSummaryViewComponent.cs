ffe11538a428ed878cec1ecbee82fbb3c1e7757c
gregeva <evang0911@gmail.com>
2023-03-17 23:52:19 -0600

    cart icon shenanigans

---
+++ onlineBookstore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using onlineBookstore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace onlineBookstore.Views.Shared.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket basket;
+
+        public CartSummaryViewComponent (Basket basketService)
+        {
+            basket = basketService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(basket);
+        }
+    }
+}
